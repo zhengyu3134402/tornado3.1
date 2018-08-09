41f6b228ae23d8936b61345c0aaf890e0569053b
zhengyu <17492730@qq.com>
2018-08-09 19:14:56 +0800

    base one

--- 01-helloworld.py
+++ 01-helloworld.py
@@ -1,4 +1,30 @@
-11111
+# -*- conding:utf-8 -*-
 
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+from tornado.options import define, options
 
-222
+define("port", default=8000, help="a base", type=int)
+
+class Index_handler(tornado.web.RequestHandler):
+
+    def get(self):
+
+        a = self.get_argument('a', 'hello')
+        self.write(a + ' friedly user!')
+
+if __name__ == "__main__":
+
+    tornado.options.parse_command_line()
+    app = tornado.web.Application([
+
+        (r"/", Index_handler),
+    ])
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
